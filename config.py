8069c1d311bf87dc4612ed09b4a2ae4553e1aacb
chdu <duchenpaul@gmail.com>
2018-11-14 18:40:02 +0800

    create folder if not exists

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+
+import toolkit_file
+
 DB_FILE = 'xcs_data.db'
 
 XCS_LIB = r'C:\Users\chdu\Desktop\Portal\Other\xshell_theme_preview\Xshell-ColorScheme-lib'
@@ -34,3 +37,6 @@ FONT = FONT_PATH + os.sep + 'DejaVuSansMono.ttf'
 
 PREVIEW_PATH = 'preview' + os.sep
 TABLE_COLONM_ORDER_LIST = [ i for i in TABLE_COLONM.split('\n') if i ]
+
+toolkit_file.create_folder(FONT_PATH)
+toolkit_file.create_folder(PREVIEW_PATH)
